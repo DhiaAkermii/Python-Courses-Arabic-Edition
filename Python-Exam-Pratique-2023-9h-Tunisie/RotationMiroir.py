0802ce6a3ce1c2308daa89ae0ea15876475e3afa
Dhia Akermi <akermidhiastudent@gmail.com>
2023-11-05 12:35:24 +0100

    exam_python_2023_9h

---
+++ Python-Exam-Pratique-2023-9h-Tunisie/RotationMiroir.py
@@ -0,0 +1,35 @@
+from PyQt5.uic import loadUi 
+from PyQt5.QtWidgets import QApplication 
+
+
+
+    
+def Rotation(ch):
+    res = ""
+    for i in range(0,len(ch)):
+        res = res + chr(97 + (ord(ch[i]) - 97 + 13) % 26)
+    return res
+
+
+def Miroir(ch):
+   return ch[::-1]
+
+def Play():
+    ch = windows.ch.text()
+    if(len(ch) == 0):
+        windows.res.setText("Veuillez introduire une chaine")
+    else:
+        miniscule = 1
+        for i in ch:
+            if(i > 'z' or i < 'a'):
+                miniscule = 0
+                break
+        if(miniscule):
+            windows.res.setText("La chaine cryptée est : "+Miroir(Rotation(ch)))
+        else:
+            windows.res.setText("Veuillez introduire une chaine valide !")
+app = QApplication([]) 
+windows = loadUi ("InterfaceRotationMiroir.ui") 
+windows.show()
+windows.crypter.clicked.connect (Play) 
+app.exec_()
